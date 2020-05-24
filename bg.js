f28878c28974b9180e7b0d519e102b4d3f485425
letsang <tsang.jacques@gmail.com>
2020-05-24 14:32:52 +0200

    ok

---
+++ bg.js
@@ -0,0 +1,15 @@
+$(function() {
+	$('body').on("keydown", function(e){
+		var keycode = e.keyCode;
+
+        switch(keycode) {
+        	case 39:
+$(this).css({'background':'url("https://vignette.wikia.nocookie.net/earthenring/images/5/56/Thar_Walk_Anim.gif/revision/latest/scale-to-width-down/340?cb=20070326214130")no-repeat center center fixed'}); 
+        	break;
+							 
+        	case 37:
+$(this).css({'background':'url("https://s7.gifyu.com/images/ezgif-3-2325131422af.gif")no-repeat center center fixed'}); 
+        	break;
+        }
+    });
+});
